647ffb679d5e26d31fb7a69c515646e056473bf1
AAKASH HANDA <AAKASH HANDA>
2018-05-06 07:30:41 +0530

    A

---
+++ first.js
@@ -0,0 +1,85 @@
+Es5
+--------------------------------------------
+string
+number
+boolean
+
+null
+undefiend
+
+var a =1 
+undefined
+var a = "kumar"
+undefined
+var a = true
+undefined
+var b = "10"
+undefined
+typeof(b)
+"string"
+var a = 10
+undefined
+typeof(a)
+"number"
+var c = true
+undefined
+typeof(c)
+"boolean"
+
+1+1
+2
+2-1
+1
+2*2
+4
+3/2
+1.5
+5%2
+1
+4%2
+0
+
+var a ="this is kumar learning"
+undefined
+var b ="  javascript"
+undefined
+a+b
+"this is kumar learning  javascript"
+
+
+var a = "kumar"
+undefined
+a.length
+5
+a.toUpperCase()
+"KUMAR"
+a.toLocaleLowerCase()
+"kumar"
+10+10
+20
+"A"+"B"
+"AB"
+10+"a"
+"10a"
+10+10+"10"
+"2010"
+"10"+10+10
+"101010"
+10+"10"+10
+"101010"
+var a = "kumar"
+undefined
+a.repeat()
+""
+a.repeat(2)
+"kumarkumar"
+
+Es6
+--------------------------------------------------
+
+let a  ="kumar"
+
+let j = "this is kumar learning"
+let k ="  javascript"
+
+let d = `this is kumar learning ${k}`
---
+++ second.js
@@ -0,0 +1,41 @@
+var a = [1,2,3,4,5,6]
+var a= ["a","b","c"]
+var c = [1,2,"vf",3,4,"eds","ds",true,true]
+
+var a =[1,2,3,"sdf","DS",5,true,"d"]
+undefined
+a[0]
+1
+a[4]
+"DS"
+a[0]
+1
+a[5]
+5
+a.length
+8
+
+var a =[1,2,3,"sdf","DS",5,true,"d"]
+undefined
+a.push(10)
+9
+a
+(9) [1, 2, 3, "sdf", "DS", 5, true, "d", 10]
+a.push("kumar")
+10
+a
+(10) [1, 2, 3, "sdf", "DS", 5, true, "d", 10, "kumar"]
+a.pop(5)
+"kumar"
+a
+(9) [1, 2, 3, "sdf", "DS", 5, true, "d", 10]
+a.pop()
+10
+a.pop(8)
+"d"
+a
+(7) [1, 2, 3, "sdf", "DS", 5, true]
+a.pop(8)
+true
+a
+(6) [1, 2, 3, "sdf", "DS", 5]
